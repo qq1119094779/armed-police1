e3870b743a329bb2e6cd8b8c8ef1cdea02397220
lixin <1119094779@qq.com>
2020-06-06 21:17:38 +0800

    添加目录

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,14 +1,13 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <div>首页</div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: "index"
 }
 </script>
 
-<style>
+<style scoped>
+
 </style>
